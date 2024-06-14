9b2098c64953a47c6b81142feb7bdc83f76cfad8
vitalii <0x1d46@gmail.com>
2024-06-14 14:22:42 +0200

    init23

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
-import { readFile } from "node:fs/promises";
+const html = `<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Vite + React + TS</title>
+  </head>
+  <body>
+    <div id="root"></div>
+    <script type="module" src="/src/main.tsx"></script>
+  </body>
+</html>`;
 
 export async function GET() {
-  const html = await readFile("../index.html", "utf8");
-  return new Response(html + "hello");
+  return new Response(html);
 }
