5c47833656485e3eaaf0b8ec4f0d58aad55a843f
vitalii <0x1d46@gmail.com>
2024-06-14 15:00:14 +0200

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+import * as cheerio from "cheerio";
+import fs from "fs";
+import path from "path";
+
+const LEN = 100;
+
+const html = fs.readFileSync(path.join("./index.html"), "utf8");
+
+const text =
+  "Чтобы отрендерить страницу с динамическим путем и закешировать её в Next.js, можно воспользоваться функцией статической генерации с динамическими роутами (getStaticPaths и getStaticProps). Вот пошаговое руководство:";
+const desc = text.length > LEN ? text.slice(0, LEN - 3) + "..." : text;
+const $ = cheerio.load(html);
+
+$(`meta[property="og:title"]`).attr("content", desc);
+
+console.log($.html());
