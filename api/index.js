930226b68172cd6c658603e771565e80858de704
vitalii <0x1d46@gmail.com>
2024-06-14 14:29:55 +0200

    init

--- api/index.js
+++ api/index.js
@@ -1,19 +1,5 @@
-const html = `<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <link rel="icon" type="image/svg+xml" href="/vite.svg" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Vite + React + TS</title>
-  </head>
-  <body>
-    <div id="root"></div>
-    <script type="module" src="/src/main.tsx"></script>
-  </body>
-</html>`;
-
 export async function GET() {
-  return new Response(html, {
-    headers: { "Content-Type": "text/html" },
+  return new Response(JSON.stringify(process.versions), {
+    // headers: { "Content-Type": "text/html" },
   });
 }
