12cb56825e1fd94e195d2f7e61c34788b5a515f8
vitalii <0x1d46@gmail.com>
2024-06-14 14:33:35 +0200

    init

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import fs from "fs";
 export async function GET() {
-  const html = fs.readFileSync("../dist/index.html", "utf8");
+  const html = fs.readFileSync("/dist/index.html", "utf8");
   return new Response(html, {
     // headers: { "Content-Type": "text/html" },
   });
