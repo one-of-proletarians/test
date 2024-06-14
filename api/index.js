ed634ff0629df22d9372a6c1f107bd3e480d93bc
vitalii <0x1d46@gmail.com>
2024-06-14 14:00:20 +0200

    init

--- api/index.js
+++ api/index.js
@@ -1,6 +1,4 @@
-// api/index.js
-
-module.exports = (req, res) => {
+export async function GET(req, res) {
   res.setHeader("Content-Type", "text/html");
-  res.status(200).send("<h1>Hello from the server-side script!</h1>");
-};
+  return res.status(200).send("<h1>Hello from the server-side script!</h1>");
+}
